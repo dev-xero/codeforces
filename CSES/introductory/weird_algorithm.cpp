f01b22f6bced952d58b3689cde5be5dcb924ec09
= <ssh.xero@gmail.com>
2024-11-30 17:02:56 +0100

    opt(CSES): Optimize weird algorithm

--- CSES/introductory/weird_algorithm.cpp
+++ CSES/introductory/weird_algorithm.cpp
@@ -5,22 +5,27 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-    long n = 0; cin >> n;
+    long n = 0;
+    cin >> n;
 
     while (n != 1)
     {
         cout << n << " ";
-        if (n % 2 == 0) {
+        if (n % 2 == 0)
+        {
             n /= 2;
-        } else {
-            n *= 3; n += 1;
+        }
+        else
+        {
+            n *= 3;
+            n += 1;
         }
     }
 
-    cout << "1" << endl;
+    cout << "1" << '\n';
 
     return 0;
 }
